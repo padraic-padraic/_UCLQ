fee9a54974f2094b7f643e082a533251e62bfebc
Padraic Calpin <padraic.calpin93@gmail.com>
2017-02-26 18:01:31 +0000

    Restructure components to make divs easier to keep track of
    Initial carousel work with placeholder gifs

--- index.php
+++ index.php
@@ -12,30 +12,49 @@
  */
 
 get_header(); ?>
-
-	<?php if ( have_posts() ) : ?>
-
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php
-				/* Include the Post-Format-specific template for the content.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'content', get_post_format() );
-			?>
-
-		<?php endwhile; ?>
-
-		<?php // _UCLQ_content_nav( 'nav-below' ); ?>
-        <?php _UCLQ_pagination(); ?>
-
-	<?php else : ?>
-
-		<?php get_template_part( 'no-results', 'index' ); ?>
-
-	<?php endif; ?>
-
-<?php get_sidebar(); ?>
+<div class="main-content">
+  <div class="container-fluid">
+  	<div class="row">
+  		<div id="story-carousel-row" class="col-sm-12">
+  			<div id="story-carousel" class="carousel slide" data-ride="carousel">
+	  			<div class="carousel-inner" role="listbox">
+				    <div class="item active">
+				      <img style="height:200px; width:100%;" src="http://i.giphy.com/ZkYiNL52YuXYY.gif" alt="...">
+				      <div class="carousel-caption">
+				        Boop
+				      </div>
+				    </div>
+				    <div class="item">
+				      <img style="height:200px; width:100%;" src="http://i.giphy.com/yhfTY8JL1wIAE.gif" alt="...">
+				      <div class="carousel-caption">
+				        Beep
+				      </div>
+				    </div>
+			    </div>
+  			</div>
+  		</div>
+  	</div>
+	<div class="row">
+	    <div id="content" class="main-content-inner col-sm-12 col-md-8">
+			<?php if ( have_posts() ) : ?>
+				<?php /* Start the Loop */ ?>
+					<?php //while ( have_posts() ) : the_post(); ?>
+						<?php
+								/* Include the Post-Format-specific template for the content.
+								 * If you want to overload this in a child theme then include a file
+								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+								 */
+								//get_template_part( 'content', get_post_format() );
+							?>
+					<?php //endwhile; ?>
+					<?php // _UCLQ_content_nav( 'nav-below' ); ?>
+			        <?php //_UCLQ_pagination(); ?>
+				<?php else : ?>
+					<?php //get_template_part( 'no-results', 'index' ); ?>
+				<?php endif; ?>
+		</div><!-- close .main-content-inner -->
+		<?php get_sidebar(); ?>
+	</div><!-- close .row -->
+  </div><!-- close .container -->
+</div><!-- close .main-content -->
 <?php get_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -6,36 +6,35 @@
  */
 ?>
 
-	</div><!-- close .main-content-inner -->
+<div class="sidebar col-sm-12 col-md-4">
 
-	<div class="sidebar col-sm-12 col-md-4">
+	<?php // add the class "panel" below here to wrap the sidebar in Bootstrap style ;) ?>
+	<div class="sidebar-padder">
 
-		<?php // add the class "panel" below here to wrap the sidebar in Bootstrap style ;) ?>
-		<div class="sidebar-padder">
+		<?php do_action( 'before_sidebar' ); ?>
+		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-			<?php do_action( 'before_sidebar' ); ?>
-			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+			<aside id="search" class="widget widget_search">
+				<?php get_search_form(); ?>
+			</aside>
 
-				<aside id="search" class="widget widget_search">
-					<?php get_search_form(); ?>
-				</aside>
+			<aside id="archives" class="widget widget_archive">
+				<h3 class="widget-title"><?php _e( 'Archives', '_UCLQ' ); ?></h3>
+				<ul>
+					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+				</ul>
+			</aside>
 
-				<aside id="archives" class="widget widget_archive">
-					<h3 class="widget-title"><?php _e( 'Archives', '_UCLQ' ); ?></h3>
-					<ul>
-						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-					</ul>
-				</aside>
+			<aside id="meta" class="widget widget_meta">
+				<h3 class="widget-title"><?php _e( 'Meta', '_UCLQ' ); ?></h3>
+				<ul>
+					<?php wp_register(); ?>
+					<li><?php wp_loginout(); ?></li>
+					<?php wp_meta(); ?>
+				</ul>
+			</aside>
 
-				<aside id="meta" class="widget widget_meta">
-					<h3 class="widget-title"><?php _e( 'Meta', '_UCLQ' ); ?></h3>
-					<ul>
-						<?php wp_register(); ?>
-						<li><?php wp_loginout(); ?></li>
-						<?php wp_meta(); ?>
-					</ul>
-				</aside>
+		<?php endif; ?>
 
-			<?php endif; ?>
-
-		</div><!-- close .sidebar-padder -->
+	</div><!-- close .sidebar-padder -->
+</div><!-- close .sidebar -->
