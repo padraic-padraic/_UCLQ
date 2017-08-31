61be4a5491690d0a1492529257d4ba1a1c8c864c
Padraic Calpin <padraic.calpin93@gmail.com>
2017-09-01 00:22:15 +0100

    Footer work, facilities query

--- footer.php
+++ footer.php
@@ -11,14 +11,19 @@
 <?php // substitute the class "container-fluid" below if you want a wider content area ?>
 	<div class="container-fluid">
 		<div class="row">
-			<div class="site-footer-inner col-sm-12 no-padded-col">
+			<div class="site-footer-inner col-sm-8 no-padded-col">
 				<div class="site-info">
 					<?php do_action( '_UCLQ_credits' ); ?>
 					<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', '_UCLQ' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', '_UCLQ' ), 'WordPress' ); ?></a>
-					<span class="sep"> | </span>
-                    <a class="credits" href="http://themekraft.com/" target="_blank" title="Themes and Plugins developed by Themekraft" alt="Themes and Plugins developed by Themekraft"><?php _e('Themes and Plugins developed by Themekraft.','_UCLQ') ?> </a>
+					<br>
+					<a class="credits" href="http://github.com/padraic-padraic/_UCLQ" target="_blank" title="Theme and Plugins developed by Padraic Calpin" alt="Theme and Plugins developed by Padraic Calpin">Theme and plugins developed by Padraic Calpin, </a>
+                    <a class="credits" href="http://themekraft.com/" target="_blank" title="Themes and Plugins developed by Themekraft" alt="Themes and Plugins developed by Themekraft"><?php _e('and based on the _tk theme, developed by Themekraft.','_UCLQ') ?> </a><br>
+                    <a class="credits" href="mailto:j.mohanty@ucl.ac.uk"> Layout and design by Aparajita Mohanty</a>
 				</div><!-- close .site-info -->
-
+			</div>
+			<div class="site-footer-inner col-sm-4 no-padded-col social-icons text-right">
+			<a href="https://twitter.com/UCLQuantum"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+			<a href="https://www.youtube.com/channel/UCuw7eoi7mRyXH6ItWfBSkLg"><i class="fa fa-youtube fa-2x" aria-hidden="true"></i></a>
 			</div>
 		</div>
 	</div><!-- close .container -->
--- index_parts/facilities.php
+++ index_parts/facilities.php
@@ -12,17 +12,35 @@
                 <h1 class="fp-title">Facilities</h1>
             </div>
             <div class="panel panel-body">
-                <div class="equal row content-inner-row">
-                    <div class="col-sm-4">
-                        <img class="img-responsive" src="http://i.giphy.com/3KMnQJcwcZB0k.gif">
-                    </div>
-                    <div class="col-sm-4">
-                        <img class="img-responsive" src="http://i.giphy.com/uiDCwmi3I3yQo.gif">
-                    </div>
-                    <div class="col-sm-4">
-                        <img class="img-responsive" src="http://i.giphy.com/qjj4xrA1STjfa.gif">
-                    </div>
+                        <?php $facility_args = array( 'posts_per_page'=>3,
+                                    'orderby' => 'rand',
+                                    'post_type'=>'uclq_facility');
+              $recent_posts = new WP_Query($facility_args);
+              if ($recent_posts->have_posts() ) {
+                while ($recent_posts->have_posts()){
+                  $recent_posts->the_post();
+                  $thumbnail = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
+        ?>
+          <div class="col-sm-4 col-xs-12">
+            <div class="col-sm-12 thumbnail text-center">
+              <img class="news-thumb" src="<?php echo $thumbnail[0];?>">
+              <a href="<?php echo get_the_permalink();?>">
+                <div class="facility_caption">
+                  <p><span style="font-weight:bold"><?php echo get_the_title();?></span><br>
+                  <?php 
+                 $post_department = array_shift(wp_get_post_terms($post->ID, 'department'));
+                  echo get_post_meta(get_the_ID(), 'facility_location', true).' in '.$post_department->name;
+                  ?>
+                  </p>
                 </div>
+              </a>
+            </div>
+          </div>
+       <?php
+               }
+               wp_reset_postdata();
+             }
+       ?>
             </div>
         </div>
     </div>
